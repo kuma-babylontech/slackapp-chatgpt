988d15e97a481943da243a8b17ec3102e31be0ab
kuma <kuma@babylon-tech.com>
2025-11-10 10:45:06 +0900

    ストリーミングでの応答処理を追加

--- app.py
+++ app.py
@@ -1,27 +1,70 @@
 import os
 import re
+import time
+from typing import Any
 
 from dotenv import load_dotenv
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import LLMResult
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+CHAT_UPDATE_INTERVAL_SEC = 1
+
 load_dotenv()
 
-app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+app = App(
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    token=os.getenv("SLACK_BOT_TOKEN"),
+    process_before_response=True,
+)
+
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts):
+        self.channel = channel
+        self.ts = ts
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
+            self.last_send_time = now
+            app.client.chat_update(
+                channel=self.channel,
+                ts=self.ts,
+                text=f"{self.message}...",
+            )
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        app.client.chat_update(
+            channel=self.channel,
+            ts=self.ts,
+            text=self.message,
+        )
 
 @app.event("app_mention")
 def handle_mention(event, say):
+    channel = event["channel"]
     thread_ts = event["ts"]
     message = re.sub(r"<@.*>", "", event["text"])
 
+    result = say("\n\nTyping...", thread_ts=thread_ts)
+    ts = result["ts"]
+
+    callback = SlackStreamingCallbackHandler(channel, ts)
     llm = ChatOpenAI(
         model_name=os.getenv("OPENAI_API_MODEL"),
         temperature=os.getenv("OPENAI_API_TEMPERATURE"),
+        streaming=True,
+        callbacks=[callback],
     )
 
-    response = llm.predict(message)
-    say(text=response, thread_ts=thread_ts)
+    llm.predict(message)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
