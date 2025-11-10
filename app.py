fe6bc82420c26485f76fadae11a12a08e3aa8877
kuma <kuma@babylon-tech.com>
2025-11-10 10:24:03 +0900

    OoenAI APIの呼び出し処理を追加

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
+import re
+
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
@@ -9,9 +12,16 @@ app = App(token=os.getenv("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
 def handle_mention(event, say):
-    user = event["user"]
     thread_ts = event["ts"]
-    say(thread_ts=thread_ts, text=f"Hello, <@{user}>!")
+    message = re.sub(r"<@.*>", "", event["text"])
+
+    llm = ChatOpenAI(
+        model_name=os.getenv("OPENAI_API_MODEL"),
+        temperature=os.getenv("OPENAI_API_TEMPERATURE"),
+    )
+
+    response = llm.predict(message)
+    say(text=response, thread_ts=thread_ts)
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
